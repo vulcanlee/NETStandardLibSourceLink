031486b1dd7dda5aab89648b5c58f4bf75bc099a
李進興 Vulcan <vulcan@miniasp.com>
2018-01-05 00:12:53 +0800

    1.0

---
+++ NETStandardLibSourceLink/SayHello.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NETStandardLib
+{
+
+    public class SayHello
+    {
+        public string Hello()
+        {
+#if NET40
+            return "Hi NET40";
+#elif NET45
+            return "Hi NET45";
+#elif NETCOREAPP1_0
+            return "Hi NETCOREAPP1_0";
+#elif NETCOREAPP2_0
+            return "Hi NETCOREAPP2_0";
+#elif NETSTANDARD2_0
+            return "Hi NETSTANDARD2_0";
+#endif
+        }
+    }
+}
